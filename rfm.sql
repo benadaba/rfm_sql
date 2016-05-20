5bd137bf1ff3041910dbaadd4e3b15d55f31abe6
Bernard Antwi Adabankah <adabankah.com@gmail.com>
2016-05-20 10:48:13 +0100

    Create rfm.sql

---
+++ rfm.sql
@@ -0,0 +1,40 @@
+/*
+START RFM
+*/
+
+/*
+RECENCY
+*/
+SELECT   cFullName, Country, dReceievedDate AS OrderDate
+FROM orders
+GROUP BY cEmailAddress
+ORDER BY dReceievedDate DESC
+LIMIT 10;
+
+
+/*
+RECENCY
+*/
+SELECT   cFullName, Country, dReceievedDate AS OrderDate
+FROM orders
+ORDER BY dReceievedDate DESC
+LIMIT 10;
+
+/*
+FREQUENCY
+*/
+SELECT  count(cEmailAddress) AS NumOfCustPurchases, cFullName, Country, dReceievedDate AS OrderDate
+FROM orders
+group by cFullName
+ORDER BY NumOfCustPurchases DESC
+LIMIT 10;
+
+
+
+/*
+MONETARY
+*/
+SELECT   cFullName, Country, dReceievedDate AS OrderDate, Total AS TotalAmtSpent
+FROM orders
+ORDER BY TotalAmtSpent DESC
+LIMIT 10;
