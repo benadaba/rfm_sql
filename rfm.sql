701815e7a8414091f9c03775b49552a7ffeadc56
Bernard Antwi Adabankah <adabankah.com@gmail.com>
2016-05-21 19:47:21 +0100

    Update rfm.sql

--- rfm.sql
+++ rfm.sql
@@ -1,33 +1,41 @@
-/*
-START RFM
-*/
+use r_orders;
 
-/*
-RECENCY
-*/
-SELECT   cFullName, Country, dReceievedDate AS OrderDate
-FROM orders
-GROUP BY cEmailAddress
-ORDER BY dReceievedDate DESC
-LIMIT 10;
+DROP VIEW IF exists recency;
+DROP VIEW IF exists frequency;
+DROP VIEW IF exists monetary;
+DROP VIEW IF exists rfm;
 
+CREATE view recency AS 
+SELECT cEmailAddress, order_dates,MAX(unix_timestamp(order_dates)) as order_seconds
+FROM 
+(
+select cEmailAddress,  str_to_date(dReceievedDate,'%d/%m/%Y %H:%i') as order_dates 
+from orders  where cEmailAddress!="" AND cEmailAddress!='NOT-DISCLOSED-BY-AMAZON' LIMIT 5000
+) as smalldatasets -- inner sellect. needs to have its own alias. you can pass the data up to the upper select. eg order_dates
+GROUP BY cEmailAddress;
+ 
+CREATE view frequency AS 
+SELECT cEmailAddress,SUM(OrderItemQuantity) AS NumOfPurchases
+FROM 
+(
+select cEmailAddress, OrderItemQuantity  from orders  where cEmailAddress!="" AND cEmailAddress!='NOT-DISCLOSED-BY-AMAZON' LIMIT 5000
+) as smalldatasets -- inner sellect. needs to have its own alias. you can pass the data up to the upper select. eg order_dates
+GROUP BY cEmailAddress;
+ 
+CREATE view monetary AS SELECT 	cEmailAddress, Currency, SUM(Total) AS TotalPurchaseValue
+FROM
+((select cEmailAddress,(Total/1.5) AS Total,"GBP" AS Currency from orders where Currency="EUR" AND cEmailAddress!="" AND cEmailAddress!='NOT-DISCLOSED-BY-AMAZON'  limit 2500)
+UNION ALL
+(select cEmailAddress,Total,Currency from orders where Currency="GBP" AND cEmailAddress!="" AND cEmailAddress!='NOT-DISCLOSED-BY-AMAZON' limit 2500)) AS common_currency
+GROUP BY cEmailAddress; 
 
-
-/*
-FREQUENCY
-*/
-SELECT  count(cEmailAddress) AS NumOfCustPurchases, cFullName, Country, dReceievedDate AS OrderDate
-FROM orders
-group by cFullName
-ORDER BY NumOfCustPurchases DESC
-LIMIT 10;
-
-
-
-/*
-MONETARY
-*/
-SELECT   cFullName, Country, dReceievedDate AS OrderDate, Total AS TotalAmtSpent
-FROM orders
-ORDER BY TotalAmtSpent DESC
-LIMIT 10;
+CREATE view rfm AS 
+SELECT rf.cEmailAddress,rf.recency,rf.frequency, m.TotalPurchaseValue as monetary FROM
+(
+SELECT r.cEmailAddress, r.order_dates as recency,f.NumOfPurchases as frequency
+FROM recency r INNER JOIN frequency f
+    ON r.cEmailAddress = f.cEmailAddress) rf INNER JOIN  monetary m 
+    ON rf.cEmailAddress=m.cEmailAddress 
+ORDER BY   recency DESC,   frequency DESC,monetary DESC;    
+-- ORDER BY   recency DESC,   frequency DESC,monetary DESC LIMIT 10; -- top 10 NEW CUSTOMERS
+-- ORDER BY   recency ASC,   frequency ASC,monetary ASC; -- OLD CUSTOMERS
